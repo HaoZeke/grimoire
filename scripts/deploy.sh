5a498cc850828d17680e696d9c6b1bf9ad60292d
HaoZeke <driger633@gmail.com>
2017-11-30 02:22:35 +0530

    ci: Add a deploy script and disable travis

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd public
+#rm -rf .git
+
+git init
+git config user.name "Semaphore CI"
+git config user.email "bot@nomail.io"
+
+git remote add upstream "git:$GITHUB_TOKEN@github.com:HaoZeke/grimoire.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
+rm -rf .git
