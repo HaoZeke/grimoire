563173a65eb7a7700ce3f40a693f0ba120aab476
HaoZeke <driger633@gmail.com>
2017-12-02 08:17:17 +0530

    CI: Use a script for semaphore-netlify

---
+++ scripts/netlify.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+zip -r website.zip dist
+
+curl -H "Content-Type: application/zip" \
+     -H "Authorization: Bearer $NETLIFY_TOKEN" \
+     --data-binary "@website.zip" \
+     https://api.netlify.com/api/v1/sites/$NETLIFY_DOMAIN/deploys
